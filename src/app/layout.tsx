c0ac639c6f9254287cfcbd2f588a167dfd97908b
dzakyahnaf <dzakyahnf@gmail.com>
2026-06-08 14:07:08 +0700

    fix: lighthouse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,10 +45,28 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          rel="stylesheet"
+        <link rel="preload" href="/api/menu" as="fetch" crossOrigin="anonymous" />
+        <link rel="preload" href="https://api-dash.olsera.co.id/img/no_data_item.png" as="image" fetchPriority="high" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap';
+                link.media = 'print';
+                link.onload = function() { this.media = 'all'; };
+                document.head.appendChild(link);
+              })();
+            `
+          }}
         />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#A8131E" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
